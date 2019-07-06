b28586750d29030c1d6dca2457a1b59a5c2d19fb
mranjon007 <mithun.bishwash.cse@ulab.edu.bd>
2019-07-07 05:55:08 +0600

    New updates for Heroku deployment

--- Page_App_Project/Page_Project/settings.py
+++ Page_App_Project/Page_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0#!hm-76yrdz#&)+rzh655sdrxko5ub1dmtz+ji1@dyx1cmizk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
